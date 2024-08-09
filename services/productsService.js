61dba65244355977d6bcc606a182535bc8e4ae7a
Annazubakha <korepovaanna@gmail.com>
2024-08-09 11:02:52 +0200

    add page and limit query to products requests

--- services/productsService.js
+++ services/productsService.js
@@ -2,7 +2,7 @@ import { Product } from "../models/product.js";
 
 export const listProducts = async (query) => {
   try {
-    const { name, category } = query;
+    const { name, category, page = 1, limit = 12 } = query;
     const filter = {};
 
     if (name) {
@@ -12,8 +12,9 @@ export const listProducts = async (query) => {
     if (category) {
       filter.category = category;
     }
+    const skip = (page - 1) * limit;
 
-    const products = await Product.find(filter);
+    const products = await Product.find(filter).skip(skip).limit(limit);
     return products;
   } catch (error) {
     return error;
