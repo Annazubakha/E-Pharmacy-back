acc11fb611d8500ffa1ccd624de36874205baa78
Annazubakha <korepovaanna@gmail.com>
2024-08-12 16:31:14 +0200

    log out logic  validate body auth

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
+import validateBody from "../helpers/validateBody.js";
 import {
   registerController,
   loginController,
+  logOutController,
 } from "../controllers/authControllers.js";
+import { authenticate } from "../middlewares/authenticate.js";
+import { loginSchema, registerSSchema } from "../models/user.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/login", loginController);
-authRouter.post("/register", registerController);
+authRouter.post("/login", validateBody(loginSchema), loginController);
+authRouter.post("/register", validateBody(registerSSchema), registerController);
+authRouter.post("/logout", authenticate, logOutController);
 
 export default authRouter;
